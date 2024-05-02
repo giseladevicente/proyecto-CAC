313bc3e1b12c2aefc91353c319c0b21d428f27cc
giseladevicente <devicentegisela@gmail.com>
2024-05-01 23:18:30 -0300

    primer commit

---
+++ assets/js/menu.js
@@ -0,0 +1,8 @@
+let btnmenu = document.getElementById("boton_menu");
+let menu = document.getElementById("menu_items");
+
+btnmenu.addEventListener("click", function() {
+    "use strict";
+    console.log('Botón de menú clickeado');
+    menu.classList.toggle('mostrar');
+});
