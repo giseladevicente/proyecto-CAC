ec737cb428d51104a93fee9055315f4610664e9e
giseladevicente <devicentegisela@gmail.com>
2024-05-16 13:41:03 -0300

    Comente validacion de registros para luego chequeargit add .

--- assets/js/validacion-registro.js
+++ assets/js/validacion-registro.js
@@ -1,22 +1,37 @@
-const formulario = document.querySelector('#formulario-registro');
 
-let validarDatos = (event) => {
-    event.preventDefault();
-}
+// document.addEventListener('DOMContentLoaded', function() {
+// console.log('El script de validación se está ejecutando');
 
-let nombre = document.querySelector('#name');
+// const formulario = document.querySelector("#formulario-registro");
 
-let valido = true;
+// if (formulario) {
+//     console.log("Se encontró el formulario correctamente");
+// } else {
+//     console.log("No se encontró el formulario. Revisa tu selector.");
+// }
 
-if (nombre.value === "" ) {
-    let errorNombre = document.querySelector("#errorName");
-    errorNombre.textContent = "El campo Nombre es obligatorio";
-    nombre.classList.add("error");
-    valido = false;
-}
 
-if (valido) {
-    formulario.submit();
-} 
+// let validarDatos = (event) => {
+//     event.preventDefault();
+//     console.log("Se activó la función validarDatos");
 
-formulario.addEventListener('submit', validarDatos);
+// let nombre = document.querySelector('#name')
+
+// console.error("nombre seleccionado:", nombre);
+// console.error("actual nombre:", nombre.value);
+
+// if (nombre.value === "" ) {
+//     let errorNombre = document.querySelector("#errorName");
+//     console.log(nombre.value)
+//     errorNombre.textContent = "El campo Nombre es obligatorio";
+//     nombre.classList.add("error");
+//     console.log("El campo nombre está vacío");
+// } else {
+//     console.log("El campo nombre está lleno");
+// }
+
+// };
+
+// formulario.addEventListener('submit', validarDatos);
+
+// });
